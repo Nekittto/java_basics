edf3b068484b337368a8ce0aca5160c127c1e9a0
nikita_kirillov <Kirillov.Niikita@yandex.ru>
2023-09-02 09:33:13 +0600

    transfer

---
+++ Primitives_IntOrInteger/src/Container.java
@@ -0,0 +1,13 @@
+public class Container {
+    private int count;
+    // Второй вариант исправления ошибки Integen count = 0;
+
+    public void addCount(int value) {
+        count = count + value;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+}
